70c297b3c66f19e348c57b6b046f5698cff77121
maherme <manuel.hernandez.mendez@gmail.com>
2021-05-20 23:16:51 +0200

    Added SumAll function.

--- ArraysSlices/sum.go
+++ ArraysSlices/sum.go
@@ -8,3 +8,16 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll takes a varying number of slices, returning a new slice containing
+// the totals of each slice passed in.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
