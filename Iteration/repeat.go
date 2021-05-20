18c056f593adafc6831114fd35f8bca95c3f98d6
maherme <manuel.hernandez.mendez@gmail.com>
2021-05-20 20:33:16 +0200

    Added comment to Repeat function.

--- Iteration/repeat.go
+++ Iteration/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat creates a string with a character repeated the number of times passed as repetitions.
 func Repeat(character string, repetitions int) string {
 	var repeated string
 	for i := 0; i < repetitions; i++ {
