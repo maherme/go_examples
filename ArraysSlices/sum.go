496dd2a70098874b41de2153b3a3930d60e067a0
maherme <manuel.hernandez.mendez@gmail.com>
2021-05-20 23:35:26 +0200

    Added SumAllTails function.

--- ArraysSlices/sum.go
+++ ArraysSlices/sum.go
@@ -20,3 +20,15 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+// SumAllTails calculates the totals of the "tails" of each slice.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
