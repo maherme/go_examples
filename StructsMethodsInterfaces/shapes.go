03938bf5e0fa0e4bec318cadf3a064d5b0ab4bda
maherme <manuel.hernandez.mendez@gmail.com>
2021-05-21 16:24:42 +0200

    Refactor adding methods.

--- StructsMethodsInterfaces/shapes.go
+++ StructsMethodsInterfaces/shapes.go
@@ -1,16 +1,25 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 // Perimeter calculates the perimeter of a rectangle.
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-// Area calculates the area of a rectangle.
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
--- StructsMethodsInterfaces/shapes_test.go
+++ StructsMethodsInterfaces/shapes_test.go
@@ -13,11 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{9.0, 3.0}
-	got := Area(rectangle)
-	want := 27.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{9.0, 3.0}
+		got := rectangle.Area()
+		want := 27.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{9}
+		got := circle.Area()
+		want := 254.46900494077323
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
