f18fae36bae4bab21dad98101e1115faa455a542
maherme <manuel.hernandez.mendez@gmail.com>
2021-05-30 15:35:46 +0200

    Added case for function.

--- Reflection/reflection_test.go
+++ Reflection/reflection_test.go
@@ -127,6 +127,23 @@ func TestWalk(t *testing.T) {
 			t.Errorf("got %v, want %v", got, want)
 		}
 	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Barcelona"}, Profile{34, "Sevilla"}
+		}
+
+		var got []string
+		want := []string{"Barcelona", "Sevilla"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 
 func assertContains(t testing.TB, haystack []string, needle string) {
